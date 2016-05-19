58fed94547dd0ee390b389f2c74dd37237c22887
crisng4 <cristina.ng4@gmail.com>
2016-05-19 23:38:52 +0200

    He realizado el ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# hola, hola
+fichero = open("/etc/passwd",'r')
+lista_usuarios = {}
 
-fd = open('/etc/passwd', 'r')
+while 1:
+    lineas = fichero.readline()
+    argumentos = lineas.split(":")
+    ##usuario = argumentos[0]
+    ##shell = argumentos[-1]
+    ##print usuario,shell[:-1]
+    lista_usuarios[argumentos[0]] = argumentos[-1][:-1]
+    if not lineas:
+        break
 
-lineas = fd.readlines()
-fd.close()
+fichero.close()
+fichero = open("/etc/passwd",'r')
+print "\nHay", len(fichero.readlines()), "usuarios\n"
+fichero.close()
 
-for linea in lineas:
-    elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
+try:
+    print "Usuario root:",lista_usuarios["root"]
+    print "Usuario imaginario:",lista_usuarios["imaginario"]
+except KeyError: #elevamos la excepción en el caso de que sea imaginario para que no salga el error por pantalla
+    print "El usuario no se ha encontrado en la lista de usuarios\n"
